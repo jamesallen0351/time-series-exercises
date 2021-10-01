9f43fa1356988fe72483f81918a608b7de5131b6
jamesallen0351 <james.allen0351@gmail.com>
2021-10-01 08:44:24 -0500

    add comments to acquire functions

--- acquire.py
+++ acquire.py
@@ -37,6 +37,35 @@ def get_store_data():
     store_data.to_csv('complete_store_data.csv', index=False)
     return store_data
 
+# another way of getting things done
+def get_sales_data():
+    url = 'https://python.zgulde.net/api/v1/sales'
+    response = requests.get(url)
+
+    filename = 'sales.csv'
+    if os.path.isfile(filename):
+        sales = pd.read_csv(filename, index_col=[0])
+    else:
+        if response.ok:
+            extracted_data = list()
+            payload = response.json()['payload']
+            max_page = payload['max_page']
+            for n in range(max_page):
+                extracted_data.extend(payload['sales'])
+                try:
+                    new_url = url[:25] + payload['next_page']
+                    print(new_url)
+                    response = requests.get(new_url)
+                    payload = response.json()['payload']
+                except:
+                    pass
+                
+            sales = pd.DataFrame(extracted_data)
+            sales.to_csv(filename)
+
+        else:
+            print(response.status_codeus_code)
+    return sales
 
 # Duetschland_Macht
 # function to bring in the german power data and convert it to a .csv
